6830177016c941c01d4f786c138c6efdf1c236d9
koyuboy <35155252+koyuboy@users.noreply.github.com>
2019-11-14 20:10:50 +0300

    Create Dosyaişlemlerideneme.java

---
+++ Dosyaişlemlerideneme.java
@@ -0,0 +1,120 @@
+
+package dosyaişlemlerideneme;
+import java.io.*;
+import java.util.*;
+public class Dosyaişlemlerideneme {
+    
+    public static String satirAl()
+	{
+		String satir = "";
+		try {
+	InputStreamReader converter = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(converter);
+			satir = in.readLine();
+		} catch (Exception e) {
+			System.out.println("Error! Exception: "+e); 
+		}
+		return satir;
+	}
+
+   
+    public static void main(String[] args) {
+        
+       // DOSYAYA YAZMA
+       /*
+        String cumle = "Bugün hava cok guzel.";
+        try{
+            FileOutputStream f  = new FileOutputStream("dosya.txt",true);
+            PrintStream yaz = new PrintStream(f);
+            yaz.println(cumle);
+            yaz.println("\"Her seçiş bir vazgeçiştir.\"");
+            yaz.print("Jean Paul Sartre");
+        }
+        catch(IOException e){
+            
+        }
+        */
+       
+/*-----------------------------------------------------------------------------------------------------------------------------------*/    
+/*-----------------------------------------------------------------------------------------------------------------------------------*/  
+
+       //  DOSYADAN SATIR SATIR OKUMA
+       /*
+       try{ //  TRY-CATCH OLMAYINCA HATA VERİYOR BURDA
+           FileInputStream fi = new FileInputStream("dosya.txt");
+           BufferedReader buf = new BufferedReader(new InputStreamReader(fi));
+           String satir;
+           while((satir = buf.readLine()) != null){
+               System.out.println(satir);
+           }
+       }
+               catch(FileNotFoundException f)       
+        {System.out.println("dosya yok");}
+          catch(IOException ioex)
+          {System.out.println("başka bir hata oldu"); }
+    */
+       
+/*-----------------------------------------------------------------------------------------------------------------------------------*/    
+/*-----------------------------------------------------------------------------------------------------------------------------------*/ 
+    
+       // DOSYADAN HARF HARF OKUMA
+       /*
+        StringBuffer icerik = new StringBuffer("");
+        try{
+            File f=new File("dosya.txt");
+            FileInputStream fi=new FileInputStream(f);
+            int k;
+            while ((k=fi.read())!=-1){
+       //append dosyadakileri silmeden sonuna eklemek
+            icerik.append((char)k);     // harfleri bir stringde birleşitiriyoruz,, sonra icerik stringini istediğimiz yerde yzdırabilriz
+           // System.out.print((char)k);  // bu şekilde yazarsakda olur,, aşşağıda icerik  stringine ihtiyacımız kalmaz;
+           // System.out.println((char)k);  // bu şekilde yazarsak harfleri tek tek  basar;
+            }
+        
+        }
+        catch(FileNotFoundException f)
+        {System.out.println("dosya yok");}
+          catch(IOException ioex)
+          {System.out.println("başka bir hata oldu"); }
+        //--
+           System.out.println("icerik :");
+           System.out.println(icerik);
+    */      
+       
+/*-----------------------------------------------------------------------------------------------------------------------------------*/    
+/*-----------------------------------------------------------------------------------------------------------------------------------*/ 
+       
+       //  DOSYADAN KELİME KELİME OKUMA
+       //  import java.util.*; -->  ekstradan bu kütüphane de lazım  StringTokenizer  için
+       /*
+       try{
+           FileInputStream fi=new FileInputStream("dosya.txt");
+            BufferedReader buf=new BufferedReader(new InputStreamReader(fi));
+            String satir;
+            StringTokenizer st;
+            while ((satir=buf.readLine())!=null){
+                st=new StringTokenizer(satir);      // satır satır alır
+                while (st.hasMoreTokens()){     // bunu eklemezsek sadece her satırın ilk kelimesini yazar,,, bu fonksiyon st de daha fazla kelime varmı diye sorar
+                   System.out.println(st.nextToken());   }      // snextToken()  satırımızdaki sıradaki kelimeye geçer
+            }
+                
+            }
+        catch(FileNotFoundException f)
+        {System.out.println("dosya yok");}
+          catch(IOException ioex)
+          {System.out.println("başka bir hata oldu"); }
+         */
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/    
+/*-----------------------------------------------------------------------------------------------------------------------------------*/ 
+
+        // 
+        System.out.print("Hello, what's your name? ");
+		String name=satirAl();
+		System.out.println("Hello "+name+"!");
+
+       
+       
+    }
+    
+}
